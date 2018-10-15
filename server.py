fa0591b7b331561bd346508463bef741c246b8f1
Jose <jakermater@gmail.com>
2018-10-15 20:32:15 +0200

    post method not working, need to study

--- server.py
+++ server.py
@@ -7,12 +7,24 @@ async def simple_response(request):
     response_obj = { 'status' : 'success' }
     return web.Response(text=json.dumps(response_obj))
 
+async def new_user(request):
+    try:
+        user = request.query['name']
+        print("Creating new user with name: " , user)
+        response_obj = { 'status' : 'success' }
+        return web.Response(text=json.dumps(response_obj), status=200)
+
+    except Exception as e:
+        response_obj = { 'status' : 'failed', 'reason': str(e) }
+        return web.Response(text=json.dumps(response_obj), status=500)
+
 
 async def build_server(loop, address, port):
     
     app = web.Application(loop=loop)
     app.router.add_route('GET', "/", simple_response)
-
+    app.router.add_route('POST',"/user", new_user)
+    # app.router.add_post('/user', new_user)   
     return await loop.create_server(app.make_handler(), address, port)
 
 
