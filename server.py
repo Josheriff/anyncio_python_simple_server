ea43dcf74bb55ccb961bfeff78b7c8b2194cd61d
Jose <jakermater@gmail.com>
2018-10-26 09:55:23 +0200

    add gitignore

--- server.py
+++ server.py
@@ -1,40 +1,23 @@
-import asyncio
-from aiohttp import web
-import json
+from sanic import Sanic
+from sanic import response
+
+app = Sanic(__name__)
+
+# app.static("/static", "./static") <- in case we want to serve statics
+
+@app.post('/post')
+async def post_handler(request):
+    return response.json({'message': 'Post method!'})
+
+@app.get('/get')
+async def get_handler(request):
+    return response.json({'message': 'Post method!'.format(request.args)})
 
 
-async def simple_response(request):
-    response_obj = { 'status' : 'success' }
-    return web.Response(text=json.dumps(response_obj))
-
-async def new_user(request):
-    try:
-        user = request.query['name']
-        print("Creating new user with name: " , user)
-        response_obj = { 'status' : 'success' }
-        return web.Response(text=json.dumps(response_obj), status=200)
-
-    except Exception as e:
-        response_obj = { 'status' : 'failed', 'reason': str(e) }
-        return web.Response(text=json.dumps(response_obj), status=500)
-
-
-async def build_server(loop, address, port):
+if __name__ == "__main__":
+    app.run(
+        debug=True,
+        host="0.0.0.0",
+        port=8000
+    )
     
-    app = web.Application(loop=loop)
-    app.router.add_route('GET', "/", simple_response)
-    app.router.add_route('POST',"/user", new_user)
-    # app.router.add_post('/user', new_user)   
-    return await loop.create_server(app.make_handler(), address, port)
-
-
-if __name__ == '__main__':
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(build_server(loop, 'localhost', 9999))
-    print("Server ready!")
-
-    try:
-        loop.run_forever()
-    except KeyboardInterrupt:
-        print("Shutting Down!")
-        loop.close()
